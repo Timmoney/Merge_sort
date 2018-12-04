a46ddef0c059b05972cceae5c2f357af99ad42bc
Ting Lin <timlin.mail@gmail.com>
2018-12-04 12:21:17 -0800

    first

---
+++ IterMergeSort.java
@@ -0,0 +1,89 @@
+class IterMergeSort{
+	public static void mergeSort(int[] array) 
+    { 
+        //if array has nothing
+        if(array == null) 
+        { 
+            return; 
+        } 
+  
+        //if there is more than 1 ele (at least two)
+        if(array.length > 1) 
+        { 
+
+            int mid = array.length / 2; 
+  
+            // Split left part 
+            int[] left = new int[mid]; 
+            for(int i = 0; i < mid; i++) 
+            { 
+                left[i] = array[i]; 
+            } 
+              
+            // Split right part 
+            int[] right = new int[array.length - mid]; 
+            for(int i = mid; i < array.length; i++) 
+            { 
+                right[i - mid] = array[i]; 
+            } 
+
+            //keep dividing until there is only 1 ele in each arrays
+            mergeSort(left); 
+            mergeSort(right); 
+  
+            int i = 0; 
+            int j = 0; 
+            int k = 0; 
+  
+            // Merge left and right arrays 
+            while(i < left.length && j < right.length) 
+            { 
+                if(left[i] < right[j]) 
+                { 
+                    array[k] = left[i]; 
+                    i++; 
+                } 
+                else
+                { 
+                    array[k] = right[j]; 
+                    j++; 
+                }
+                //move to the next element 
+                k++; 
+            } 
+            // Collect remaining elements 
+            while(i < left.length) 
+            { 
+                array[k] = left[i]; 
+                i++; 
+                k++; 
+            } 
+            while(j < right.length) 
+            { 
+                array[k] = right[j]; 
+                j++; 
+                k++; 
+            } 
+        } 
+    } 
+
+    public static void main(String[] args) 
+    { 
+        int arr[] = {12, 11, 13, 5, 6, 7}; 
+        int i=0; 
+        System.out.println("Given array is"); 
+  
+        for(i=0; i<arr.length; i++) 
+            System.out.print(arr[i]+" "); 
+  
+        mergeSort(arr); 
+  
+        System.out.println("\n"); 
+        System.out.println("Sorted array is"); 
+  
+        for(i=0; i<arr.length; i++) 
+            System.out.print(arr[i]+" ");
+
+        System.out.println(); 
+    } 
+} 
